c754736331a8b7e40f1d0c3dd4537d55c2130629
GabrielMontAraujo <graubx25@gmail.com>
2022-10-11 18:56:39 -0300

    Add Couting JS

---
+++ script.js
@@ -0,0 +1,9 @@
+var data = 0
+function increment() {
+  data = data + 1
+  document.getElementById('lbQtde').innerText = data
+}
+function decrement() {
+  data = data - 1
+  document.getElementById('lbQtde').innerText = data
+}
