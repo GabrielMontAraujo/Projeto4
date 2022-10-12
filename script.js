c9d7545c62efe0b7a959c174fdabe44a98af0ead
GabrielMontAraujo <graubx25@gmail.com>
2022-10-12 01:56:09 -0300

    Add CartModal

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var data = 0
+var price = document.getElementById('valueDesc').textContent
+var qtde = document.getElementById('lbQtde').textContent
+
 function increment() {
   data = data + 1
   document.getElementById('lbQtde').innerText = data
@@ -8,4 +11,25 @@ function decrement() {
     data = data - 1
     document.getElementById('lbQtde').innerText = data
   }
+}
+
+function productSale() {
+  var sale = data * price
+  console.log(sale)
+}
+
+const btnModal = document.querySelector('.btn-primary')
+btnModal.addEventListener('click', openModal)
+
+function openModal(e) {
+  const modalEl = document.querySelector('#modalExemplo')
+  modalEl.classList.add('visible')
+}
+
+const btnClose = document.querySelectorAll('#closeModal')
+btnClose.forEach(fechaE1 => fechaE1.addEventListener('click', closeModal))
+
+function closeModal(e) {
+  const modalEl = document.querySelector('#modalExemplo')
+  modalEl.classList.remove('visible')
 }
